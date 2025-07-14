b83cb6e6399cf77562e3ec9532121728020ab123
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-07-14 10:00:58 +0900

    update: Folia警告を入れた

--- src/main/kotlin/com/nekozouneko/preventSpawnerChange/PreventSpawnerChange.kt
+++ src/main/kotlin/com/nekozouneko/preventSpawnerChange/PreventSpawnerChange.kt
@@ -3,12 +3,23 @@ package com.nekozouneko.preventSpawnerChange
 import org.bukkit.plugin.java.JavaPlugin
 
 class PreventSpawnerChange : JavaPlugin() {
+    val isFolia = isClassExists("io.papermc.paper.threadedregions.RegionizedServer") || isClassExists("io.papermc.paper.threadedregions.RegionizedServerInitEvent")
 
     override fun onEnable() {
-        // Plugin startup logic
+        if (isFolia) {
+            logger.warning("[Warning] It appears to be running on Folia.\n" +
+                    "Folia is still experimental and may have errors.")
+        }
     }
 
-    override fun onDisable() {
-        // Plugin shutdown logic
+    override fun onDisable() {}
+
+    private fun isClassExists(clazz: String) : Boolean {
+        try {
+            Class.forName(clazz)
+            return true
+        }catch (_: ClassNotFoundException){
+            return false
+        }
     }
 }
